43556a7109e52d88517b84b2efbdbea1d36143f1
Fardias <fardias_alf@gmail.com>
2025-04-23 12:37:21 +0700

    undisplay contact section

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 const App = () => {
@@ -181,7 +181,7 @@ const App = () => {
       <About />
       <Skills />
       <Projects />
-      <Contact />
+      {/* <Contact /> */}
       <Footer />
     </div>
   );
